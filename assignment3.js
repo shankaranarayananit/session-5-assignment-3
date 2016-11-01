28625bc7946422832515e6b50f163f9c4edb0110
shankaranarayananit <shankara.88it@gmail.com>
2016-11-02 01:35:27 +0530

    Add files via upload

---
+++ assignment3.js
@@ -0,0 +1,13 @@
+﻿var Employee = {
+    Name: "Aru",
+    Age: 25,
+    salary: 20000,
+    address: {
+        city: "chennai",
+        state: "Tamilnadu",
+        Pincode: 600001
+    }
+};
+console.log("The Employee name is : " + Employee['Name'] + " and age is : " + Employee['Age'] +
+    " and salary is " + Employee.salary+ " and employee address:-> city : " +Employee.address.city+
+    ", state : "+Employee.address.state+", pincode : "+Employee.address.Pincode+".");
